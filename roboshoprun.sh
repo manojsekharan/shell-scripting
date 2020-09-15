407d4c7a4afc38347bc7002e1413a3f4b2cfc544
manoj <maninmanoj@gmail.com>
2020-09-15 15:08:08 +0530

    this is a tes

---
+++ roboshoprun.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+#case $USER_ID in
+#    0)
+#     echo -e "\e[33mStarting installation\e[0m"
+#    ;;
+#    *)
+#      echo -e "\e[1;31mYou have to be root to execute the command\e[0m"
+#    ;;
+#    esac
+
+if [ USER_ID -eq 0 ]; then
+  echo -e  "\e[33mStarting installation"
+else
+  echo -e "\e[33mYou have to be root to execute the command\e[0m"
+fi
+
+
+
